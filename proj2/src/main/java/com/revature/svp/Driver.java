1cc3e52c9cbe37ba5a709304368f10d54e3dba94
Emaku <emaku@eagles.bridgewater.edu>
2018-08-17 14:00:38 -0400

    Emmanuel Updated Changes. Added TestNG for SVP Location, and Servlet, And an HTML
    Signed-off-by: Emaku <emaku@eagles.bridgewater.edu>

--- proj2/src/main/java/com/revature/svp/Driver.java
+++ proj2/src/main/java/com/revature/svp/Driver.java
@@ -1,6 +1,7 @@
 package com.revature.svp;
 
 import java.io.File;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,12 +12,21 @@ public class Driver {
 	static WebDriver d = null;
 
 	public static void main(String[] args) {
-		launchApp();
-		loginSVP();
-		closeApp();
-		launchApp();
-		loginTrainer();
-		closeApp();
+//		launchApp();
+//		loginSVP();
+//		moveToLocationTab();
+//		clickLocationBar();
+//		expandFirstLocation();
+//		collapseFirstLocation();
+//
+//		addLocation();
+		
+		launchResults();
+
+//		closeApp();
+//		launchApp();
+//		loginTrainer();
+//		closeApp();
 
 	}
 
@@ -29,35 +39,144 @@ public class Driver {
 	}
 
 	public static void loginSVP() {
-		
+
+		boolean expand = true;
+		while (expand) {
+			try {
+				Thread.sleep(500);
+				d.findElement(By.name("email")).sendKeys("svp@revature.com");
+				d.findElement(By.name("password")).sendKeys("p@$$w0rd");
+				d.findElement(By.name("submit")).click();
+				System.out.println("Finally Log in!");
+				expand = false;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Nope");
+			} catch (Exception e) {
+				System.out.println("Naw");
+
+			}
+		}
+
+	}
+
+	public static void loginTrainer() {
+
+		boolean expand = true;
+		while (expand) {
+			try {
+				Thread.sleep(500);
+				d.findElement(By.name("email")).sendKeys("test.trainer@revature.com");
+				d.findElement(By.name("password")).sendKeys("p@$$w0rd");
+				d.findElement(By.name("submit")).click();
+				System.out.println("Finally Log in!");
+				expand = false;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Nope");
+			} catch (Exception e) {
+				System.out.println("Naw");
+
+			}
+		}
+
+	}
+
+	public static void moveToLocationTab() {
+
+		boolean expand = true;
+		while (expand) {
+			try {
+				Thread.sleep(500);
+				d.findElement(By.id("mat-tab-label-0-2")).click();
+				System.out.println("Finally moved!");
+				expand = false;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Nope");
+			} catch (Exception e) {
+				System.out.println("Naw");
+
+			}
+		}
+
+	}
+
+	public static void clickLocationBar() {
+
+		boolean expand = true;
+		while (expand) {
+			try {
+				Thread.sleep(500);
+				d.findElement(By.id("mat-expansion-panel-header-1")).click();
+				System.out.println("Finally collapse");
+				expand = false;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Nope");
+			} catch (Exception e) {
+				System.out.println("Naw");
+
+			}
+		}
+		while (!expand) {
+			try {
+				Thread.sleep(500);
+				d.findElement(By.id("mat-expansion-panel-header-1")).click();
+				System.out.println("Finally expand");
+				expand = true;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Nope");
+			} catch (Exception e) {
+				System.out.println("Naw");
+
+			}
+		}
+
+	}
+
+	public static void expandFirstLocation() {
+		d.findElement(By.id("mat-expansion-panel-header-2")).click();
+	}
+
+	public static void collapseFirstLocation() {
+		d.findElement(By.id("mat-expansion-panel-header-2")).click();
+	}
+
+	public static void addLocation() {
+
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(500);
+			d.findElement(By.className("cdk-overlay-container")).click();
+//			d.findElement(By.id("cdk-overlay-23")).click();
+//			d.findElement(By.id("mat-input-3")).sendKeys("FarAWAY");
+			System.out.println("Finally Clicked Add Location");
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		d.findElement(By.name("email")).sendKeys("svp@revature.com");
-		d.findElement(By.name("password")).sendKeys("p@$$w0rd");
-		d.findElement(By.name("submit")).click();
 
-		
-	}
-public static void loginTrainer() {
-		
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		d.findElement(By.name("email")).sendKeys("test.trainer@revature.com");
-		d.findElement(By.name("password")).sendKeys("p@$$w0rd");
-		d.findElement(By.name("submit")).click();
 
-		
 	}
+	
+	public static void launchResults() {
+		File chrome = new File("src/main/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", chrome.getAbsolutePath());
+		d = new ChromeDriver();
+		d.get("C:\\Users\\LiL'Birdman\\Documents\\Project\\Revature\\Project2\\proj2\\test-output\\Default suite\\Default test.html");
+	}
+	
 
 	public static void closeApp() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		d.quit();
 	}
 }
